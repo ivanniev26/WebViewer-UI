ivanniev26/WebViewer-UI

commit 4efed92fb0fae4de6d15e71ff1b912a24a314229

[bugfix] resolves #179 (#193)
* [bugfix] resolves #179

Position sorting now sorts using the closest edge to the top.
Page rotation amount in radians is now available in the state.
Changed sort strategy API to accept a contextual parameter.

* Reverted sort strategy breaking changes.
Changed strategy to get the rotation value from helper instead.
Changed rotation state from radians to orientation value.

* Reverted missed time strategy change.

src/components/NotesPanel/NotesPanel.js
@@ -221,6 +221,7 @@ const mapStatesToProps = state => ({
   sortStrategy: selectors.getSortStrategy(state),
   isDisabled: selectors.isElementDisabled(state, 'notesPanel'),
   pageLabels: selectors.getPageLabels(state),
+  pageRotation: selectors.getRotation(state),
   customNoteFilter: selectors.getCustomNoteFilter(state)
 });
 

src/constants/sortStrategies.js
@@ -1,12 +1,30 @@
 import i18next from 'i18next';
 import dayjs from 'dayjs';
+import orientationManager from 'helpers/orientationManager';
+import { rotateRad } from 'helpers/rotate';
 import getLatestActivityDate from 'helpers/getLatestActivityDate';
 
 const sortStrategies = {
   position: {
     getSortedNotes: notes => notes.sort((a, b) => {
       if (a.PageNumber === b.PageNumber) {
-        return a.Y - b.Y;
+        const rotation = orientationManager.getRotationRad(a.PageNumber);
+        const center = orientationManager.getDocumentCenter(a.PageNumber);
+
+        // Simulated with respect to the document origin
+        const rotatedA = [
+          rotateRad(center.x, center.y, a.X, a.Y, rotation),
+          rotateRad(center.x, center.y, a.X + a.Width, a.Y + a.Height, rotation),
+        ];
+        const rotatedB = [
+          rotateRad(center.x, center.y, b.X, b.Y, rotation),
+          rotateRad(center.x, center.y, b.X + b.Width, b.Y + b.Height, rotation),
+        ];
+
+        const smallestA = rotatedA.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+        const smallestB = rotatedB.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+
+        return smallestA - smallestB;
       }
       return a.PageNumber - b.PageNumber;
     }),

src/event-listeners/onRotationUpdated.js
@@ -1,1 +1,6 @@
-export default () => {};
+import actions from 'actions';
+
+export default dispatch => (e, rotation) => {
+  dispatch(actions.setRotation(rotation));
+  $(document).trigger('rotationChanged', [rotation]);
+};

src/helpers/orientationManager.js
@@ -1,0 +1,25 @@
+import core from 'core';
+
+class OrientationManager {
+    getDocumentCenter = pageNumber => {
+        const { width, height } = core.getPageInfo(pageNumber);
+
+        return { x: width / 2, y: height / 2 };
+    }
+    getRotationRad = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const radians = (4 - orientation) * (Math.PI / 2);
+
+        return radians;
+    }
+    getRotationDeg = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const degrees = (4 - orientation) * 90;
+
+        return degrees;
+    }
+}
+
+export default new OrientationManager();

src/helpers/rotate.js
@@ -1,0 +1,20 @@
+export const rotateRad = (cx, cy, x, y, radians) => {
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx; // xcos(theta) + ysin(theta)
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy; // -ysin(theta) + xcos(theta)
+
+    return { x: nx, y: ny };
+};
+
+export const rotateDeg = (cx, cy, x, y, angle) => {
+    const radians = (Math.PI/180) * angle;
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx;
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy;
+
+    return { x: nx, y: ny };
+};

src/redux/actions/internalActions.js
@@ -59,6 +59,7 @@ export const setActiveToolGroup = toolGroup => ({ type: 'SET_ACTIVE_TOOL_GROUP',
 export const setNotePopupId = id => ({ type: 'SET_NOTE_POPUP_ID', payload: { id } });
 export const setFitMode = fitMode => ({ type: 'SET_FIT_MODE', payload: { fitMode } });
 export const setZoom = zoom => ({ type: 'SET_ZOOM', payload: { zoom } });
+export const setRotation = rotation => ({ type: 'SET_ROTATION', payload: { rotation } });
 export const setDisplayMode = displayMode => ({ type: 'SET_DISPLAY_MODE', payload: { displayMode } });
 export const setCurrentPage = currentPage => ({ type: 'SET_CURRENT_PAGE', payload: { currentPage } });
 export const setFullScreen = isFullScreen => ({ type: 'SET_FULL_SCREEN', payload: { isFullScreen } });

src/redux/initialState.js
@@ -125,6 +125,7 @@ export default {
     isNoteEditing: false,
     fitMode: '',
     zoom: 1,
+    rotation: 0,
     displayMode: 'Single',
     currentPage: 1,
     sortStrategy: 'position',

src/redux/reducers/viewerReducer.js
@@ -66,6 +66,8 @@ export default initialState => (state = initialState, action) => {
       return { ...state, fitMode: payload.fitMode };
     case 'SET_ZOOM':
       return { ...state, zoom: payload.zoom };
+    case 'SET_ROTATION':
+      return { ...state, rotation: payload.rotation }
     case 'SET_DISPLAY_MODE':
       return { ...state, displayMode: payload.displayMode };
     case 'SET_CURRENT_PAGE':

commit 24ea5399f1e10ca91b4d23417e1096f98a8d0a39

Fixed regression from #193

src/components/NotesPanel/NotesPanel.js
@@ -56,6 +56,7 @@ class NotesPanel extends React.PureComponent {
 
   onDocumentUnloaded = () => {
     this.visibleNoteIds.clear();
+    this.rootAnnotations = [];
     this.setState({ notesToRender: [] });
   }
 