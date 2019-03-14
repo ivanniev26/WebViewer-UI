ivanniev26/WebViewer-UI

commit 4efed92fb0fae4de6d15e71ff1b912a24a314229

[bugfix] resolves #179 (#193)
* [bugfix] resolves #179

Position sorting now sorts using the closest edge to the top.
Page rotation amount in radians is now available in the state.
Changed sort strategy API to accept a contextual parameter.

* Reverted sort strategy breaking changes.
Changed strategy to get the rotation value from helper instead.
Changed rotation state from radians to orientation value.

* Reverted missed time strategy change.

src/components/NotesPanel/NotesPanel.js
@@ -221,6 +221,7 @@ const mapStatesToProps = state => ({
   sortStrategy: selectors.getSortStrategy(state),
   isDisabled: selectors.isElementDisabled(state, 'notesPanel'),
   pageLabels: selectors.getPageLabels(state),
+  pageRotation: selectors.getRotation(state),
   customNoteFilter: selectors.getCustomNoteFilter(state)
 });
 

src/constants/sortStrategies.js
@@ -1,12 +1,30 @@
 import i18next from 'i18next';
 import dayjs from 'dayjs';
+import orientationManager from 'helpers/orientationManager';
+import { rotateRad } from 'helpers/rotate';
 import getLatestActivityDate from 'helpers/getLatestActivityDate';
 
 const sortStrategies = {
   position: {
     getSortedNotes: notes => notes.sort((a, b) => {
       if (a.PageNumber === b.PageNumber) {
-        return a.Y - b.Y;
+        const rotation = orientationManager.getRotationRad(a.PageNumber);
+        const center = orientationManager.getDocumentCenter(a.PageNumber);
+
+        // Simulated with respect to the document origin
+        const rotatedA = [
+          rotateRad(center.x, center.y, a.X, a.Y, rotation),
+          rotateRad(center.x, center.y, a.X + a.Width, a.Y + a.Height, rotation),
+        ];
+        const rotatedB = [
+          rotateRad(center.x, center.y, b.X, b.Y, rotation),
+          rotateRad(center.x, center.y, b.X + b.Width, b.Y + b.Height, rotation),
+        ];
+
+        const smallestA = rotatedA.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+        const smallestB = rotatedB.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+
+        return smallestA - smallestB;
       }
       return a.PageNumber - b.PageNumber;
     }),

src/event-listeners/onRotationUpdated.js
@@ -1,1 +1,6 @@
-export default () => {};
+import actions from 'actions';
+
+export default dispatch => (e, rotation) => {
+  dispatch(actions.setRotation(rotation));
+  $(document).trigger('rotationChanged', [rotation]);
+};

src/helpers/orientationManager.js
@@ -1,0 +1,25 @@
+import core from 'core';
+
+class OrientationManager {
+    getDocumentCenter = pageNumber => {
+        const { width, height } = core.getPageInfo(pageNumber);
+
+        return { x: width / 2, y: height / 2 };
+    }
+    getRotationRad = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const radians = (4 - orientation) * (Math.PI / 2);
+
+        return radians;
+    }
+    getRotationDeg = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const degrees = (4 - orientation) * 90;
+
+        return degrees;
+    }
+}
+
+export default new OrientationManager();

src/helpers/rotate.js
@@ -1,0 +1,20 @@
+export const rotateRad = (cx, cy, x, y, radians) => {
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx; // xcos(theta) + ysin(theta)
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy; // -ysin(theta) + xcos(theta)
+
+    return { x: nx, y: ny };
+};
+
+export const rotateDeg = (cx, cy, x, y, angle) => {
+    const radians = (Math.PI/180) * angle;
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx;
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy;
+
+    return { x: nx, y: ny };
+};

src/redux/actions/internalActions.js
@@ -59,6 +59,7 @@ export const setActiveToolGroup = toolGroup => ({ type: 'SET_ACTIVE_TOOL_GROUP',
 export const setNotePopupId = id => ({ type: 'SET_NOTE_POPUP_ID', payload: { id } });
 export const setFitMode = fitMode => ({ type: 'SET_FIT_MODE', payload: { fitMode } });
 export const setZoom = zoom => ({ type: 'SET_ZOOM', payload: { zoom } });
+export const setRotation = rotation => ({ type: 'SET_ROTATION', payload: { rotation } });
 export const setDisplayMode = displayMode => ({ type: 'SET_DISPLAY_MODE', payload: { displayMode } });
 export const setCurrentPage = currentPage => ({ type: 'SET_CURRENT_PAGE', payload: { currentPage } });
 export const setFullScreen = isFullScreen => ({ type: 'SET_FULL_SCREEN', payload: { isFullScreen } });

src/redux/initialState.js
@@ -125,6 +125,7 @@ export default {
     isNoteEditing: false,
     fitMode: '',
     zoom: 1,
+    rotation: 0,
     displayMode: 'Single',
     currentPage: 1,
     sortStrategy: 'position',

src/redux/reducers/viewerReducer.js
@@ -66,6 +66,8 @@ export default initialState => (state = initialState, action) => {
       return { ...state, fitMode: payload.fitMode };
     case 'SET_ZOOM':
       return { ...state, zoom: payload.zoom };
+    case 'SET_ROTATION':
+      return { ...state, rotation: payload.rotation }
     case 'SET_DISPLAY_MODE':
       return { ...state, displayMode: payload.displayMode };
     case 'SET_CURRENT_PAGE':

commit 011830a25b8fe25e2dbf17e4e236bbc9ba7ae8b2

[new] Measurement overlay (#201)
* [update] change preloading to load pdfnet.res (#191)

* [update] Ungroup header item (#192)

* Ungroup tool button when updateTool API is called with buttonGroup as null

* Default insert position of ungrouped button is before miscTools

* [bugfix] resolves #179 (#193)

* [bugfix] resolves #179

Position sorting now sorts using the closest edge to the top.
Page rotation amount in radians is now available in the state.
Changed sort strategy API to accept a contextual parameter.

* Reverted sort strategy breaking changes.
Changed strategy to get the rotation value from helper instead.
Changed rotation state from radians to orientation value.

* Reverted missed time strategy change.

* first version

* the second version. Updating data in the measurement overlay while  creating the annotation or moving the annotation

* remove commected out code

* add translation for measurement overlay. Refactor/modify based on Matt's feedback

* update to the latest core, something is broken and tried to fix that

* clean up logic

* almost finished, only thing seems left is calculating correct angle when a IPath annotaiton has more than three vertices

* Fixed regression from #193

* update angle calculation for polyline and polygon when they have different number of vertices

* Enable measurement overlay with enableMeasurement API

src/apis/disableMeasurement.js
@@ -3,6 +3,6 @@ import actions from 'actions';
 import core from 'core';
 
 export default store => () => {
-  store.dispatch(actions.disableElement('measurementToolGroupButton', PRIORITY_ONE));
+  store.dispatch(actions.disableElements([ 'measurementToolGroupButton', 'measurementOverlay' ], PRIORITY_ONE));
   core.setToolMode('AnnotationEdit');
 };

src/apis/enableMeasurement.js
@@ -2,5 +2,5 @@ import { PRIORITY_ONE } from 'constants/actionPriority';
 import actions from 'actions';
 
 export default store => () => {
-  store.dispatch(actions.enableElement('measurementToolGroupButton', PRIORITY_ONE));
+  store.dispatch(actions.enableElements([ 'measurementToolGroupButton', 'measurementOverlay' ], PRIORITY_ONE));
 };

src/apis/index.js
@@ -70,6 +70,7 @@ import isToolDisabled from './isToolDisabled';
 import setNotesPanelSort from './setNotesPanelSort';
 import addSortStrategy from './addSortStrategy';
 import updateOutlines from './updateOutlines';
+import updateTool from './updateTool';
 import print from './print';
 import showWarningMessage from './showWarningMessage';
 import enableRedaction from './enableRedaction';
@@ -149,6 +150,7 @@ export default {
   setNotesPanelSort,
   addSortStrategy,
   updateOutlines,
+  updateTool,
   print, 
   getZoomLevel,
   setZoomLevel,

src/apis/updateTool.js
@@ -1,0 +1,39 @@
+import setHeaderItems from '../apis/setHeaderItems';
+import { updateTool } from '../redux/actions/exposedActions';
+
+export default store => (toolName, properties) => {
+  if (properties.buttonGroup === null){
+    setHeaderItems(store)(function(header){
+      let alreadyExist = false;
+      header.getHeader('default').headers.default.forEach(function(element){
+        if (element.toolName === toolName){
+          alreadyExist = true;
+        }
+      });
+      if (!alreadyExist){
+        header.getHeader('default').get('miscToolGroupButton').insertBefore({
+          type: 'toolButton',
+          toolName: toolName,
+          hidden: [ 'tablet', 'mobile' ]
+        });
+        header.getHeader('tools').get('miscToolGroupButton').insertBefore({
+          type: 'toolButton',
+          toolName: toolName,
+          hidden: [ 'desktop' ]
+        });
+      }
+    });
+  } else {
+    setHeaderItems(store)(function(header){
+      const returnDefaultHeader = header.headers.default.filter(function(element){
+        return element.toolName !== toolName;
+      });
+      header.getHeader('default').update(returnDefaultHeader);
+      const returnToolsHeader = header.headers.tools.filter(function(element){
+        return element.toolName !== toolName;
+      });
+      header.getHeader('tools').update(returnToolsHeader);
+    });
+  }
+  store.dispatch(({ type: 'UPDATE_TOOL', payload: { toolName, properties } }));
+};

src/components/App/App.js
@@ -13,6 +13,7 @@ import PageNavOverlay from 'components/PageNavOverlay';
 import ToolsOverlay from 'components/ToolsOverlay';
 import SignatureOverlay from 'components/SignatureOverlay';
 import CursorOverlay from 'components/CursorOverlay';
+import MeasurementOverlay from 'components/MeasurementOverlay';
 import DocumentContainer from 'components/DocumentContainer';
 import LeftPanel from 'components/LeftPanel';
 import SearchPanel from 'components/SearchPanel';
@@ -102,6 +103,7 @@ class App extends React.PureComponent {
           <SignatureOverlay />
           <CursorOverlay />
           <ZoomOverlay />
+          <MeasurementOverlay />
 
           <AnnotationPopup />
           <TextPopup />

src/components/MeasurementOverlay/MeasurementOverlay.js
@@ -1,0 +1,277 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+import { connect } from 'react-redux';
+
+import Icon from 'components/Icon';
+
+import core from 'core';
+import { mapAnnotationToKey, mapToolNameToKey, getDataWithKey } from 'constants/map';
+import getClassName from 'helpers/getClassName';
+import actions from 'actions';
+import selectors from 'selectors';
+
+import './MeasurementOverlay.scss';
+
+class MeasurementOverlay extends React.PureComponent {
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    isDisabled: PropTypes.bool,
+    openElement: PropTypes.func.isRequired,
+    closeElement: PropTypes.func.isRequired,
+    activeToolName: PropTypes.string.isRequired,
+    t: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      annotation: null
+    };
+  }
+
+  componentDidMount() {
+    core.addEventListener('mouseMove', this.onMouseMove);
+    core.addEventListener('annotationSelected', this.onAnnotationSelected);
+    core.addEventListener('annotationChanged', this.onAnnotationChanged);
+  }
+  
+  componentDidUpdate(prevProps) {
+    if (prevProps.activeToolName !== this.props.activeToolName) {
+      const { openElement, closeElement } = this.props;
+      if (this.isMeasurementTool(this.props.activeToolName)) {
+        openElement('measurementOverlay');
+      } else {
+        closeElement('measurementOverlay');
+      }
+    }
+
+    if (prevProps.isOpen && !this.props.isOpen) {
+      this.setState({ annotation: null });
+    }
+  }
+
+  componentWillUnmount() {
+    core.removeEventListener('mouseMove', this.onMouseMove);
+    core.removeEventListener('annotationSelected', this.onAnnotationSelected);
+    core.removeEventListener('annotationChanged', this.onAnnotationChanged);
+  }
+
+  onMouseMove = () => {
+    const { activeToolName, openElement } = this.props;
+    const tool = core.getTool(activeToolName);
+
+    if (this.state.annotation) {
+      this.forceUpdate();
+    } else if (
+      this.isMeasurementTool(activeToolName) && 
+      tool.annotation &&
+      this.shouldShowInfo(tool.annotation)
+    ) {
+      openElement('measurementOverlay');
+      this.setState({ annotation: tool.annotation });
+    }
+  }
+
+  onAnnotationSelected = (e, annotations, action) => {
+    const { openElement, closeElement } = this.props;
+    
+    if (
+      action === 'selected' && 
+      annotations.length === 1 && 
+      this.isMeasurementAnnotation(annotations[0])
+    ) {
+      this.setState({ annotation: annotations[0] });
+      openElement('measurementOverlay');
+    } else if (
+      action === 'deselected' &&
+      !core.isAnnotationSelected(this.state.annotation)
+    ) {
+      closeElement('measurementOverlay');
+    }
+  }
+
+  onAnnotationChanged = (e, annotations, action) => {
+    // measurement overlay will open and show the annotation information when we are creating an annotation using measurement tools
+    // since by default we don't auto select an annotation after it's created, we close the overlay here to avoid the confusion 
+    // where no annotation is selected but measurement overlay shows the information about the annotation we were creating
+    if (
+      action === 'add' &&
+      annotations.length === 1 &&
+      annotations[0] && this.state.annotation
+    ) {
+      this.props.closeElement('measurementOverlay');
+    }
+  }
+
+  isMeasurementAnnotation = annotation => ['distanceMeasurement', 'perimeterMeasurement', 'areaMeasurement'].includes(mapAnnotationToKey(annotation));
+
+  isMeasurementTool = toolName => ['distanceMeasurement', 'perimeterMeasurement', 'areaMeasurement'].includes(mapToolNameToKey(toolName));
+
+  shouldShowInfo = annotation => {
+    const key = mapAnnotationToKey(annotation);
+
+    let showInfo;
+    if (key === 'perimeterMeasurement' || key === 'areaMeasurement') {
+      // for polyline and polygon, there's no useful information we can show if it has no vertices or only one vertex.
+      showInfo = annotation.getPath().length > 1;
+    } else if (key === 'distanceMeasurement') {
+      showInfo = true;
+    }
+
+    return showInfo;
+  }
+
+  getAngleInRadians = (pt1, pt2, pt3) => {
+    let angle;
+
+    if (pt1 && pt2) {
+      if (pt3) {
+        // calculate the angle using Law of cosines
+        const AB = Math.sqrt(Math.pow(pt2.x - pt1.x, 2) + Math.pow(pt2.y - pt1.y, 2));    
+        const BC = Math.sqrt(Math.pow(pt2.x - pt3.x, 2) + Math.pow(pt2.y - pt3.y, 2)); 
+        const AC = Math.sqrt(Math.pow(pt3.x - pt1.x, 2) + Math.pow(pt3.y - pt1.y, 2));
+        angle = Math.acos((BC*BC + AB*AB - AC*AC) / (2*BC*AB));
+      } else {
+        // if there are only two points returns the angle in the plane (in radians) between the positive x-axis and the ray from (0,0) to the point (x,y)
+        angle = Math.atan2(pt2.y - pt1.y, pt2.x - pt1.x);
+        // keep the angle range between 0 and Math.PI / 2
+        angle = Math.abs(angle); 
+        angle = angle > Math.PI / 2 ? Math.PI - angle : angle;
+      }
+    }
+
+    return angle;
+  }
+
+  getNumberOfDecimalPlaces = annotation => 
+    annotation.Precision === 1 
+      ? 0 
+      : annotation.Precision.toString().split('.')[1].length;
+
+  renderTitle = () => {
+    const { t } = this.props;
+    const key = mapAnnotationToKey(this.state.annotation);
+    const { icon } = getDataWithKey(key);
+
+    const keyTitleMap = {
+      distanceMeasurement: t('option.measurementOverlay.distanceMeasurement'),
+      perimeterMeasurement: t('option.measurementOverlay.perimeterMeasurement'),
+      areaMeasurement: t('option.measurementOverlay.areaMeasurement')
+    };
+
+    return (
+      <div className="measurement__title">
+        {icon &&
+          <Icon className="measurement__icon" glyph={icon} />
+        }
+        {keyTitleMap[key]}
+      </div>
+    );
+  }
+
+  renderValue = () => {
+    const { annotation } = this.state;
+    const { t } = this.props;
+    const key = mapAnnotationToKey(annotation);
+
+    const keyDisplayNameMap = {
+      distanceMeasurement: t('option.measurementOverlay.distance'),
+      perimeterMeasurement: t('option.measurementOverlay.perimeter'),
+      areaMeasurement: t('option.measurementOverlay.area')
+    };
+
+    return (
+      <div className="measurement__value">
+        {keyDisplayNameMap[key]}: {annotation.getContents()}
+      </div>
+    );
+  }
+
+  renderDeltas = () => {
+    const { annotation } = this.state;
+    const angle = this.getAngleInRadians(annotation.Start, annotation.End);
+    const unit = annotation.Scale[1][1];
+    const decimalPlaces = this.getNumberOfDecimalPlaces(annotation);
+    const distance = parseFloat(annotation.getContents());
+    const deltaX = Math.abs(distance * Math.cos(angle)).toFixed(decimalPlaces); 
+    const deltaY = Math.abs(distance * Math.sin(angle)).toFixed(decimalPlaces);
+    
+    return (
+      <div className="measurement__deltas">
+        <div className="measurement__deltas--X">&Delta;X: {deltaX} {unit}</div>
+        <div className="measurement__deltas--Y">&Delta;Y: {deltaY} {unit}</div>
+      </div>
+    );
+  }
+
+  renderAngle = () => {
+    const { annotation } = this.state;
+    const key = mapAnnotationToKey(annotation);
+    const getIPathAnnotationPts = annotation => {
+      const path = annotation.getPath();
+      const length = path.length;
+      return [path[length - 3], path[length - 2], path[length - 1]];
+    };
+    const keyPtMap = {
+      distanceMeasurement: ({ Start, End }) => [Start, End],
+      perimeterMeasurement: getIPathAnnotationPts,
+      areaMeasurement: getIPathAnnotationPts
+    };
+    const pts = keyPtMap[key](annotation).filter(pt => !!pt);
+    
+    let angle = this.getAngleInRadians(...pts);
+    if (angle) {
+      const decimalPlaces = this.getNumberOfDecimalPlaces(annotation);
+      angle = (angle / Math.PI * 180).toFixed(decimalPlaces);
+    }
+
+    return (
+      angle !== undefined && 
+      <div className="measurement__angle">
+        {this.props.t('option.measurementOverlay.angle')}: {angle}&deg;
+      </div>
+    );
+  }
+
+  render() {
+    const { annotation } = this.state;
+    const { isDisabled, t } = this.props;
+    const className = getClassName('Overlay MeasurementOverlay', this.props);
+    const key = mapAnnotationToKey(annotation);
+
+    if (isDisabled || !annotation) {
+      return null;
+    }
+
+    return (
+      <div className={className} data-element="measurementOverlay">
+        {this.renderTitle()}
+        <div className="measurement__scale">
+          {t('option.measurementOverlay.scale')}: {annotation.Measure.scale}
+        </div>
+        <div className="measurement__precision">
+        {t('option.measurementOverlay.precision')}: {annotation.Precision}
+        </div>
+        {this.renderValue()}
+        {key === 'distanceMeasurement' &&
+          this.renderDeltas()
+        }
+        {this.renderAngle()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isOpen: selectors.isElementOpen(state, 'measurementOverlay'),
+  isDisabled: selectors.isElementDisabled(state, 'measurementOverlay'),
+  activeToolName: selectors.getActiveToolName(state),
+});
+
+const mapDispatchToProps = {
+  openElement: actions.openElement,
+  closeElement: actions.closeElement
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(translate()(MeasurementOverlay));

src/components/MeasurementOverlay/MeasurementOverlay.scss
@@ -1,0 +1,38 @@
+@import '../../constants/styles';
+@import '../../constants/overlay';
+
+.MeasurementOverlay {
+  @extend %overlay;
+  flex-direction: column;
+  bottom: 20px;
+  right: 20px;
+  font-size: 16px;
+  min-width: 215px;
+
+  @include mobile {
+    bottom: 8px;
+    right: 8px;
+    padding: 8px;
+    font-size: 14px;
+    z-index: 50;
+  }
+
+  & > * {
+    margin: 3px;
+  }
+}
+
+.measurement__title {
+  .measurement__icon {
+    margin-right: 5px;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.measurement__deltas {
+  display: flex;
+  justify-content: space-between
+}

src/components/MeasurementOverlay/index.js
@@ -1,0 +1,3 @@
+import MeasurementOverlay from './MeasurementOverlay';
+
+export default MeasurementOverlay;

src/components/NotesPanel/NotesPanel.js
@@ -56,6 +56,7 @@ class NotesPanel extends React.PureComponent {
 
   onDocumentUnloaded = () => {
     this.visibleNoteIds.clear();
+    this.rootAnnotations = [];
     this.setState({ notesToRender: [] });
   }
 
@@ -221,6 +222,7 @@ const mapStatesToProps = state => ({
   sortStrategy: selectors.getSortStrategy(state),
   isDisabled: selectors.isElementDisabled(state, 'notesPanel'),
   pageLabels: selectors.getPageLabels(state),
+  pageRotation: selectors.getRotation(state),
   customNoteFilter: selectors.getCustomNoteFilter(state)
 });
 

src/constants/sortStrategies.js
@@ -1,12 +1,30 @@
 import i18next from 'i18next';
 import dayjs from 'dayjs';
+import orientationManager from 'helpers/orientationManager';
+import { rotateRad } from 'helpers/rotate';
 import getLatestActivityDate from 'helpers/getLatestActivityDate';
 
 const sortStrategies = {
   position: {
     getSortedNotes: notes => notes.sort((a, b) => {
       if (a.PageNumber === b.PageNumber) {
-        return a.Y - b.Y;
+        const rotation = orientationManager.getRotationRad(a.PageNumber);
+        const center = orientationManager.getDocumentCenter(a.PageNumber);
+
+        // Simulated with respect to the document origin
+        const rotatedA = [
+          rotateRad(center.x, center.y, a.X, a.Y, rotation),
+          rotateRad(center.x, center.y, a.X + a.Width, a.Y + a.Height, rotation),
+        ];
+        const rotatedB = [
+          rotateRad(center.x, center.y, b.X, b.Y, rotation),
+          rotateRad(center.x, center.y, b.X + b.Width, b.Y + b.Height, rotation),
+        ];
+
+        const smallestA = rotatedA.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+        const smallestB = rotatedB.reduce((smallest, current) => current.y < smallest ? current.y : smallest, Number.MAX_SAFE_INTEGER);
+
+        return smallestA - smallestB;
       }
       return a.PageNumber - b.PageNumber;
     }),

src/event-listeners/onRotationUpdated.js
@@ -1,1 +1,6 @@
-export default () => {};
+import actions from 'actions';
+
+export default dispatch => (e, rotation) => {
+  dispatch(actions.setRotation(rotation));
+  $(document).trigger('rotationChanged', [rotation]);
+};

src/helpers/orientationManager.js
@@ -1,0 +1,25 @@
+import core from 'core';
+
+class OrientationManager {
+    getDocumentCenter = pageNumber => {
+        const { width, height } = core.getPageInfo(pageNumber);
+
+        return { x: width / 2, y: height / 2 };
+    }
+    getRotationRad = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const radians = (4 - orientation) * (Math.PI / 2);
+
+        return radians;
+    }
+    getRotationDeg = pageNumber => {
+        const orientation = core.getRotation(pageNumber);
+
+        const degrees = (4 - orientation) * 90;
+
+        return degrees;
+    }
+}
+
+export default new OrientationManager();

src/helpers/rotate.js
@@ -1,0 +1,20 @@
+export const rotateRad = (cx, cy, x, y, radians) => {
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx; // xcos(theta) + ysin(theta)
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy; // -ysin(theta) + xcos(theta)
+
+    return { x: nx, y: ny };
+};
+
+export const rotateDeg = (cx, cy, x, y, angle) => {
+    const radians = (Math.PI/180) * angle;
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx;
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy;
+
+    return { x: nx, y: ny };
+};

src/helpers/setDefaultDisabledElements.js
@@ -90,7 +90,7 @@ const disableElementsIfToolBarDisabled = dispatch => {
 const disableElementsIfMeasurementsDisabled = dispatch => {
   const measurementsDisabled = !getHashParams('enableMeasurement', false);
   if (measurementsDisabled) {
-    dispatch(actions.disableElement('measurementToolGroupButton', PRIORITY_ONE));
+    dispatch(actions.disableElements(['measurementToolGroupButton', 'measurementOverlay'], PRIORITY_ONE));
   }
 };
 

src/index.js
@@ -88,7 +88,7 @@ if (window.CanvasRenderingContext2D) {
   if (state.advanced.preloadWorker && state.advanced.engineType === engineTypes.PDFNETJS) {
     if (state.document.pdfType !== 'wait') {
       getBackendPromise(state.document.pdfType).then(pdfType => {
-        window.CoreControls.preloadPDFWorker(pdfType, {}, {});
+        window.CoreControls.initPDFWorkerTransports(pdfType, {}, null);
       });
     }
 
@@ -213,6 +213,7 @@ if (window.CanvasRenderingContext2D) {
           toggleFullScreen: apis.toggleFullScreen,
           unregisterTool: apis.unregisterTool(store),
           updateOutlines: apis.updateOutlines(store),
+          updateTool: apis.updateTool(store),
           loadedFromServer: false,
           serverFailed: false,
         };

src/redux/actions/internalActions.js
@@ -59,6 +59,7 @@ export const setActiveToolGroup = toolGroup => ({ type: 'SET_ACTIVE_TOOL_GROUP',
 export const setNotePopupId = id => ({ type: 'SET_NOTE_POPUP_ID', payload: { id } });
 export const setFitMode = fitMode => ({ type: 'SET_FIT_MODE', payload: { fitMode } });
 export const setZoom = zoom => ({ type: 'SET_ZOOM', payload: { zoom } });
+export const setRotation = rotation => ({ type: 'SET_ROTATION', payload: { rotation } });
 export const setDisplayMode = displayMode => ({ type: 'SET_DISPLAY_MODE', payload: { displayMode } });
 export const setCurrentPage = currentPage => ({ type: 'SET_CURRENT_PAGE', payload: { currentPage } });
 export const setFullScreen = isFullScreen => ({ type: 'SET_FULL_SCREEN', payload: { isFullScreen } });

src/redux/initialState.js
@@ -125,6 +125,7 @@ export default {
     isNoteEditing: false,
     fitMode: '',
     zoom: 1,
+    rotation: 0,
     displayMode: 'Single',
     currentPage: 1,
     sortStrategy: 'position',

src/redux/reducers/viewerReducer.js
@@ -66,6 +66,8 @@ export default initialState => (state = initialState, action) => {
       return { ...state, fitMode: payload.fitMode };
     case 'SET_ZOOM':
       return { ...state, zoom: payload.zoom };
+    case 'SET_ROTATION':
+      return { ...state, rotation: payload.rotation }
     case 'SET_DISPLAY_MODE':
       return { ...state, displayMode: payload.displayMode };
     case 'SET_CURRENT_PAGE':

src/redux/selectors/exposedSelectors.js
@@ -50,6 +50,7 @@ export const getZoom = state => state.viewer.zoom;
 export const getDisplayMode = state => state.viewer.displayMode;
 export const getCurrentPage = state => state.viewer.currentPage;
 export const getSortStrategy = state => state.viewer.sortStrategy;
+export const getRotation = state => state.viewer.rotation;
 export const getNoteDateFormat = state => state.viewer.noteDateFormat;
 export const isFullScreen = state => state.viewer.isFullScreen;
 export const doesDocumentAutoLoad = state => state.viewer.doesAutoLoad;